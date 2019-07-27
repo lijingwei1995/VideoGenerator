5874e00a31567add5ffde39f281136af80eae7c5
risan95 <lijingwei@outlook.com>
2019-07-27 10:30:28 +0900

    a

--- gui.py
+++ gui.py
@@ -8,7 +8,23 @@ class mainwindow(QMainWindow):
         self.set_actions()
 
     def set_actions(self):
-        self.B1.clicked.connect(self.buttonclicked)
+        # 定义导航按钮组
+        self.nav_buttons = [
+            self.B1, self.B2, self.B3
+        ]
+        self.current_button = self.B1
+        self.current_button.setEnabled(False)
 
-    def buttonclicked(self):
-        QMessageBox.about(self, "click", "yes")
+        # 换页事件
+        for button in self.nav_buttons:
+            button.clicked.connect(self.change_page)
+
+    def change_page(self):
+        # QMessageBox.about(self, "click", "yes")
+        button_page = self.sender().property("page_num")
+        self.stackedWidget.setCurrentIndex(button_page - 1)
+        # 按钮状态变换
+        self.current_button.setEnabled(True)
+        self.current_button = self.sender()
+        self.current_button.setEnabled(False)
+
