a86d1907246a1b4e612f5b9ebfce663fbed0d995
risan95 <lijingwei@outlook.com>
2019-07-28 16:44:31 +0900

    f

--- gui.py
+++ gui.py
@@ -64,17 +64,51 @@ class mainwindow(QMainWindow):
     def page1_B_SELECT_clicked(self):
         row = self.P1_LW_NEWS.currentRow()
         if row != -1:
-            print(self.news_list[row][1])
+            self.news_title = self.news_list[row][0]
+            self.pickup_url = self.news_list[row][1]
+            # 页面2读取新闻封面
+            self.P2_WEV.load(QUrl(self.pickup_url))
+            # 将标题传给页面2的Line Edit
+            self.P2_LE_TITLE.setText(self.news_title)
+            self.P2_L_TITLE.setText(self.news_title)
+            # 页面1 -> 页面2
+            self.stackedWidget.setCurrentIndex(1)
+            self.B1.setEnabled(True)
+            self.B2.setEnabled(False)
 
     ##### page2 #####
     def set_actions_page2(self):
+        # 生成WebEngineView及Mask
         self.P2_WEV = QWebEngineView(self.page_2)
         self.P2_WEV.setGeometry(QtCore.QRect(0, 30, 640, 270))
-        self.P2_WEV.load(QUrl('https://news.yahoo.co.jp/pickup/6331434'))
+        self.P2_WEV.hide() 
+        self.P2_WEV_MASK = QWidget(self.page_2)
+        self.P2_WEV_MASK.setGeometry(QtCore.QRect(0, 30, 640, 270))
         self.P2_WEV.loadFinished.connect(self.page2_WEV_load_finished)
+        # 生成标题label
+        self.P2_LE_TITLE = QtWidgets.QLineEdit(self.page_2)
+        self.P2_LE_TITLE.setGeometry(QtCore.QRect(0, 350, 641, 41))
+        self.P2_LE_TITLE.setObjectName("P2_LE_TITLE")
+        self.P2_LE_TITLE.textChanged.connect(lambda: self.P2_L_TITLE.setText(self.sender().text()))
+        self.label_5 = QtWidgets.QLabel(self.page_2)
+        self.label_5.setGeometry(QtCore.QRect(0, 65, 625, 90))
+        self.label_5.setStyleSheet( "font: 28pt \"Noto Sans Mono CJK\";\n"
+                                    "color:white;\n"
+                                    "background-color: rgba(0,0,0,0.5);")
+        self.label_5.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label_5.setObjectName("label_5")
+        self.label_5.setText("  日网评论：")
+        self.P2_L_TITLE = QtWidgets.QLabel(self.page_2)
+        self.P2_L_TITLE.setGeometry(QtCore.QRect(0, 155, 625, 90))
+        self.P2_L_TITLE.setStyleSheet(  "font: 28pt \"Noto Sans Mono CJK\";\n"
+                                        "color:white;\n"
+                                        "background-color: rgba(0,0,0,0.5);")
+        self.P2_L_TITLE.setAlignment(QtCore.Qt.AlignCenter)
+        self.P2_L_TITLE.setObjectName("P2_L_TITLE")
         
 
     def page2_WEV_load_finished(self):
         self.P2_WEV.page().runJavaScript("window.scrollTo(0,215);")
+        self.P2_WEV.show()
+        self.P2_B_PRINT.setText("打印")
         self.P2_B_PRINT.setEnabled(True)
-        
